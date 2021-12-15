305a192d857fea5294f48a84541e5f8be8d92b84
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-12-15 11:26:22 -0800

    feat: remove prod

--- prod/utils/auth-header.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authHeader = void 0;
-exports.authHeader = {
-    Authorization: `Bearer ${process.env.CROWDIN_PERSONAL_TOKEN}`,
-};
