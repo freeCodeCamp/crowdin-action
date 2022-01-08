92ee6892e32f0ca1b4f630601cff9b7a73a35a06
Shaun Hamilton <shauhami020@gmail.com>
2022-01-08 13:01:51 +0000

    feat(project): add rust-in-replit config

--- dist/index.js
+++ dist/index.js
@@ -16712,6 +16712,7 @@ const commit_changes_1 = __nccwpck_require__(9561);
 const convert_chinese_1 = __nccwpck_require__(4777);
 const generate_config_1 = __nccwpck_require__(5996);
 const hide_curriculum_strings_1 = __nccwpck_require__(2620);
+const hide_replit_strings_1 = __nccwpck_require__(7574);
 const hide_specific_string_1 = __nccwpck_require__(7141);
 const pull_request_1 = __nccwpck_require__(3653);
 const remove_deleted_files_1 = __nccwpck_require__(3592);
@@ -16763,6 +16764,9 @@ const validate_environment_1 = __nccwpck_require__(3724);
             break;
         case "hide-curriculum-strings":
             yield (0, hide_curriculum_strings_1.hideCurriculumStrings)(projectId);
+            break;
+        case "hide-renpy-strings":
+            yield (0, hide_replit_strings_1.hideReplitStrings)(projectId);
             break;
         case "hide-string":
             if (!process.env.FILE_NAME || !process.env.STRING_CONTENT) {
@@ -17032,6 +17036,65 @@ const hideCurriculumStrings = (projectId) => __awaiter(void 0, void 0, void 0, f
     console.log("complete");
 });
 exports.hideCurriculumStrings = hideCurriculumStrings;
+
+
+/***/ }),
+
+/***/ 7574:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.hideReplitStrings = void 0;
+const files_1 = __nccwpck_require__(6455);
+const strings_1 = __nccwpck_require__(8927);
+/**
+ * Module to hide strings for Rust in Replit translations.
+ * Hides any string that starts with `#` or is a code block.
+ *
+ * @param {number} projectId The Crowdin project ID for strings to hide.
+ */
+const hideReplitStrings = (projectId) => __awaiter(void 0, void 0, void 0, function* () {
+    const projectFiles = yield files_1.CrowdinFilesHelper.getFiles(projectId);
+    if (!projectFiles || !projectFiles.length) {
+        return;
+    }
+    for (const file of projectFiles) {
+        const fileStrings = yield strings_1.CrowdinStringHelper.getStrings(projectId, file.fileId);
+        if (!fileStrings || !fileStrings.length) {
+            continue;
+        }
+        for (const string of fileStrings) {
+            if (string.data.text.startsWith("#")) {
+                if (string.data.isHidden) {
+                    console.log(`string already hidden: ${string.data.text}`);
+                    continue;
+                }
+                console.log(`hiding string: ${string.data.text}`);
+                yield strings_1.CrowdinStringHelper.changeHiddenStatus(projectId, string.data.id, true);
+            }
+            else {
+                if (!string.data.isHidden) {
+                    console.log(`string already visible: ${string.data.text}`);
+                    continue;
+                }
+                console.log(`keeping string: ${string.data.text}`);
+                yield strings_1.CrowdinStringHelper.changeHiddenStatus(projectId, string.data.id, false);
+            }
+        }
+    }
+});
+exports.hideReplitStrings = hideReplitStrings;
 
 
 /***/ }),
