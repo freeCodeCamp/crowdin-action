3b860f1fadfa3759699334624a1848ec71166911
a2937 <a.rcottrill521@gmail.com>
2024-02-08 19:12:19 -0500

    Remove error catcher
    Co-authored-by: Mama Naomi <nhcarrigan@gmail.com>

--- src/plugins/lowercase-directories.ts
+++ src/plugins/lowercase-directories.ts
@@ -11,13 +11,8 @@ export const lowercaseDirectories = async (directories: string[]) => {
     if (directory.toLocaleLowerCase() !== directory) {
       const oldPath = join(process.cwd(), directory);
       const newPath = join(process.cwd(), directory.toLocaleLowerCase());
-      await rename(oldPath, newPath, (err) => {
-        if (err) {
-          console.error("Error making directory lowercase:", err);
-        } else {
-          console.log(`${directory} has been made lowercase`);
-        }
-      });
+      console.log(`Renaming ${directory}`);
+      await rename(oldPath, newPath);
     }
   }
 };
