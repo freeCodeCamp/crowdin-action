0321aea04c64f99ea359778baff6cb7bd2be670b
Aaron <a.rcottrill521@gmail.com>
2024-02-29 16:35:27 -0500

    feat: make the lowercase-directories plugin recursive

--- src/plugins/lowercase-directories.ts
+++ src/plugins/lowercase-directories.ts
@@ -1,18 +1,49 @@
-import { rename } from "fs/promises";
+import { Dirent } from "fs";
+import { readdir, rename } from "fs/promises";
 import { join } from "path";
+
+async function readAllFiles(dir: string): Promise<string[]> {
+  const files: Dirent[] = await readdir(dir, { withFileTypes: true });
+  let allFiles: string[] = [];
+  for (const file of files) {
+    const filePath = join(dir, file.name);
+    if (file.isDirectory()) {
+      const subFiles = await readAllFiles(filePath);
+      allFiles = allFiles.concat(subFiles);
+    } else {
+      allFiles.push(filePath);
+    }
+  }
+  return allFiles;
+}
+
+// Recursive function to process directories and subdirectories
+const processDirectory = async (dirPath: string) => {
+  // Get all files recursively
+  const allFiles = await readAllFiles(dirPath);
+
+  // Lowercase directory names
+  for (const filePath of allFiles) {
+    const dirPath = filePath.split("/").slice(0, -1).join("/");
+    const dirName = filePath.split("/").pop();
+    const lowercasedDirName = dirName?.toLowerCase();
+    if (lowercasedDirName === undefined) {
+      throw new Error("Lowercased DirName Required");
+    }
+
+    if (lowercasedDirName !== dirName) {
+      const newPath = join(dirPath, lowercasedDirName);
+      console.log(`Renaming directory ${filePath} to ${newPath}`);
+      await rename(filePath, newPath);
+    }
+  }
+};
 
 /**
  *
- * @param {string[]} directories The directories that must be sorted through.
+ * @param {string} directory The directory tree that must be sorted through.
  */
-export const lowercaseDirectories = async (directories: string[]) => {
-  console.info("Getting file list...");
-  for (const directory of directories) {
-    if (directory.toLocaleLowerCase() !== directory) {
-      const oldPath = join(process.cwd(), directory);
-      const newPath = join(process.cwd(), directory.toLocaleLowerCase());
-      console.log(`Renaming ${directory}`);
-      await rename(oldPath, newPath);
-    }
-  }
+export const lowercaseDirectories = (directory: string) => {
+  console.info("Getting directory list...");
+  processDirectory(directory);
 };
